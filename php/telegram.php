8af2ca0c5b1c1d1135178ccef768a48b4b167296
wildcreator13 <58700280+wildcreator13@users.noreply.github.com>
2021-10-24 20:19:19 +0300

    main-page

---
+++ php/telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "2067269434:AAGAlGEoyIsSGEWtzns51qqJHJknuzlYrXk";
+$chat_id = "-625032286";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: thanks.html');
+} else {
+  echo "Error";
+}
+?>
